6bb3b319868a53c22e7b000ef9fe10b6c90c692f
Jens <jens.papenhagen@web.de>
2023-02-14 20:04:14 +0100

    cleanup measuring methode, for better mentaining

--- src/main/java/de/papenhagen/service/MeasuringService.java
+++ src/main/java/de/papenhagen/service/MeasuringService.java
@@ -13,7 +13,7 @@ public class MeasuringService {
     @Inject
     InfoCrawler infoCrawler;
 
-    // WGS84 ellipsoid
+    //radius of Earth in kilometer WGS84
     private static final double EQUATOR_RADIUS = 6378.137;
 
     public double callableMeasuring(final Double plz1, final Double plz2) throws IllegalArgumentException {
@@ -22,21 +22,24 @@ public class MeasuringService {
         final Root location1 = infoCrawler.callBackend(plz1);
         final Root location2 = infoCrawler.callBackend(plz2);
 
-        final double phi1 = Math.toRadians(location1.getLat());
-        final double lambda1 = Math.toRadians(location1.getLon());
-        final double phi2 = Math.toRadians(location2.getLat());
-        final double lambda2 = Math.toRadians(location2.getLon());
+        final double lat01 = location1.getLat();
+        final double lon01 = location1.getLon();
 
-        final double angleDistanceRadians = Math.acos(
-                Math.max(
-                        Math.min(
-                                Math.sin(phi1) * Math.sin(phi2)
-                                        + Math.cos(phi1) * Math.cos(phi2) * Math.cos(lambda2 - lambda1),
-                                1.0
-                        ),
-                        -1.0
-                )
-        );
+        final double lat02 = location2.getLat();
+        final double lon02 = location2.getLon();
+
+        final double dLat = Math.toRadians(lat02 - lat01);
+        final double dLon = Math.toRadians(lon02 - lon01);
+
+        final double latDelta = Math.sin(dLat / 2);
+        final double lonDelta = Math.sin(dLon / 2);
+
+        final double lat01Radios = Math.toRadians(lat01);
+        final double lat02Radios = Math.toRadians(lat02);
+        final double a = (latDelta * latDelta) + (lonDelta * lonDelta * Math.cos(lat01Radios) * Math.cos(lat02Radios));
+
+        final double angleDistanceRadians = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
 
         return EQUATOR_RADIUS * angleDistanceRadians;
     }
